8959a22017cfc35db9ac666a4ee01a88636b1a6b
Ralph Huang <rhuang5885@gmail.com>
2019-01-03 20:07:55 -0500

    LoginPage Revamped

--- HTML/loginPage/loginPage.js
+++ HTML/loginPage/loginPage.js
@@ -1,4 +1,4 @@
-var images = [
+let images = [
     "https://lh3.googleusercontent.com/_mothnvw-dZRi7sVFutIHxLIJkC_o-4-6rznkoobfVN1-yGng4SVXkNOhj7VpCHS22NxtKqMnnzx1JYx1f5ahww0GFzxNNzpRlosW4NoLKpY4hDWT560g8XgtknhKuDFpxTZaukEPw=w2400",
     "https://lh3.googleusercontent.com/mdVwtqs-K-ktMiIyc6ly1LoW31m47lcjpxybaSRD8uTKrwjHX2FimCPdsWQK1Ipfyb2iHLGvZjEcTpnTFZVacJDEMslyVrVJiw7fzKsz349ng87Fj2TTrkcF0MskhuGA8bnKXVNjBg=w2400",
     "https://lh3.googleusercontent.com/kSAGsTo0TjhUhKimsF-f6_-Z_Rg3BXaK7FW8TLd02iaYKtMdlJa2mUQu-nWtp-nsBRMOxJc_sSqwifESEtNsJCK_JkMpogDnFM6a_AvS4bxszirOGAfNrBiL3w-6Eae3P48LPvtIfw=w2400",
